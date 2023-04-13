b5aef573cfbf3ad769b0c2eb444eb5dcca7d4980
phen0m1nal <nmnashrf@gmail.com>
2023-04-14 03:11:16 +0500

    things added

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -1,40 +1,34 @@
 import {
-  ALL_PRODUCT_REQUEST,
-  ALL_PRODUCT_SUCCESS,
-  ALL_PRODUCT_FAIL,
-} from "../constants/productConstants";
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    ALL_PRODUCT_FAIL,
+} from "../constants/productConstants"
 
-import axios from "axios";
+import axiosInstance from "../../api/axios"
 
 //Fetch All Products
-export const getProducts = id => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: ALL_PRODUCT_REQUEST,
-    });
-    const {
-      getProduct: { productInfo },
-    } = getState();
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${productInfo.token}`,
-      },
-    };
+export const getProducts = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ALL_PRODUCT_REQUEST,
+        })
+        const {
+            getProduct: { productInfo },
+        } = getState()
 
-    const { data } = await axios.get(`/api/products/getAllProducts`, config);
+        const { data } = await axiosInstance().get(`/products/getAllProducts`)
 
-    dispatch({
-      type: ALL_PRODUCT_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ALL_PRODUCT_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
+        dispatch({
+            type: ALL_PRODUCT_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: ALL_PRODUCT_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
+    }
+}
