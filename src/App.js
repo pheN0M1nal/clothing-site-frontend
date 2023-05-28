8c76598fa72b6d4fcd01c2fbf4c9f6b44d3f36f1
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-05-28 23:19:58 +0500

    Latest Commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,74 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 
-import { ToastContainer } from "react-toastify"
-import SignIn from "./pages/StateManagement/SignIn"
-import SignInAdmin from "./pages/StateManagement/SignInAdmin"
+import { ToastContainer } from "react-toastify";
+import SignIn from "./pages/StateManagement/SignIn";
+import SignInAdmin from "./pages/StateManagement/SignInAdmin";
 
-import SignUp from "./pages/StateManagement/SignUp"
-import ForgotPassword from "./pages/StateManagement/ForgotPassword"
-import Home from "./pages/Home"
-import Navbar from "./Navigation/Navbar"
-import Footer from "./Navigation/Footer"
-import Profile from "./pages/StateManagement/Profile"
-import Products from "./pages/Products/Products"
-import AllProducts from "./pages/adminPages/AllProducts"
-import ProductView from "./pages/Products/ProductView"
-import Cart from "./pages/Products/Cart"
-import AdminDashboard from "./pages/adminPages/AdminDashboard"
-import AllUsers from "./pages/adminPages/AllUsers"
-import AllDesigners from "./pages/adminPages/AllDesigners"
-import AllAdmins from "./pages/adminPages/AllAdmins"
-import Billiing from "./pages/Products/Billiing"
-import OrderPlace from "./pages/Products/OrderPlace"
-import Dashboard from "./dashboard/pages/Dashboard"
-import NotFound from "./pages/NotFound"
-import { AddProduct } from "./pages/adminPages/AddProduct"
-import CreateShop from "./pages/otherForms/CreateShop"
-import RegisterDeisgner from "./pages/otherForms/RegisterDeisgner"
+import SignUp from "./pages/StateManagement/SignUp";
+import ForgotPassword from "./pages/StateManagement/ForgotPassword";
+import Home from "./pages/Home";
+import Navbar from "./Navigation/Navbar";
+import Footer from "./Navigation/Footer";
+import Profile from "./pages/StateManagement/Profile";
+import Products from "./pages/Products/Products";
+import AllProducts from "./pages/adminPages/AllProducts";
+import ProductView from "./pages/Products/ProductView";
+import Cart from "./pages/Products/Cart";
+import AdminDashboard from "./pages/adminPages/AdminDashboard";
+import AllUsers from "./pages/adminPages/AllUsers";
+import AllDesigners from "./pages/adminPages/AllDesigners";
+import AllAdmins from "./pages/adminPages/AllAdmins";
+import Billiing from "./pages/Products/Billiing";
+import OrderPlace from "./pages/Products/OrderPlace";
+import Dashboard from "./dashboard/pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import { AddProduct } from "./pages/adminPages/AddProduct";
+import CreateShop from "./pages/otherForms/CreateShop";
+import RegisterDeisgner from "./pages/otherForms/RegisterDeisgner";
 
 function App() {
-    return (
-        <>
-            <ToastContainer />
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPassword />}
-                    />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/allProducts" element={<Products />} />
-                    <Route path="/product/:id" element={<ProductView />} />
-                    <Route path="/cart" element={<Cart />} />
-                    {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-                    <Route path="/admin/allUsers" element={<AllUsers />} />
-                    <Route
-                        path="/admin/allDesigners"
-                        element={<AllDesigners />}
-                    />
-                    <Route path="/admin/allAdmins" element={<AllAdmins />} />
-                    <Route path="/admin/addProduct" element={<AddProduct />} />
+  return (
+    <>
+      <ToastContainer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/allProducts" element={<Products />} />
+          <Route path="/product/:id" element={<ProductView />} />
+          <Route path="/cart" element={<Cart />} />
+          {/* <Route path="/admin" element={<AdminDashboard />} /> */}
+          <Route path="/admin/allUsers" element={<AllUsers />} />
+          <Route path="/admin/allDesigners" element={<AllDesigners />} />
+          <Route path="/admin/allAdmins" element={<AllAdmins />} />
+          <Route path="/admin/addProduct" element={<AddProduct />} />
 
-                    <Route path="/admin/products" element={<AllProducts />} />
+          <Route path="/admin/products" element={<AllProducts />} />
 
-                    <Route path="/admin/stats" element={<Dashboard />} />
+          <Route path="/admin/stats" element={<Dashboard />} />
 
-                    <Route path="/billing" element={<Billiing />} />
-                    <Route path="/sign-in" element={<SignIn />} />
-                    <Route path="/admin-sign-in" element={<SignInAdmin />} />
+          <Route path="/billing" element={<Billiing />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/admin-sign-in" element={<SignInAdmin />} />
 
-                    <Route path="/sign-up" element={<SignUp />} />
-                    <Route path="/order-place" element={<OrderPlace />} />
-                    <Route exact path="/admin" element={<AdminDashboard />} />
-                    <Route
-                        exact
-                        path="/register-designer"
-                        element={<RegisterDeisgner />}
-                    />
-                    <Route exact path="/create-shop" element={<CreateShop />} />
-                    <Route exact path="*" element={<NotFound />} />
-                </Routes>
-                {/* <Footer /> */}
-            </Router>
-        </>
-    )
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/order-place" element={<OrderPlace />} />
+          <Route exact path="/admin" element={<AdminDashboard />} />
+          <Route
+            exact
+            path="/register-designer"
+            element={<RegisterDeisgner />}
+          />
+          <Route exact path="/create-shop" element={<CreateShop />} />
+          <Route exact path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
-export default App
+export default App;
